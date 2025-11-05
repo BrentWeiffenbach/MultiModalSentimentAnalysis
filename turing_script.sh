50c33931291cfb767dea71acc1194a7ecf7aff55
Brent Weiffenbach <rweiff2022@gmail.com>
2025-11-05 13:55:40 -0500

    update turing script

--- turing_script.sh
+++ turing_script.sh
@@ -2,17 +2,26 @@
 #SBATCH -N 1                      # allocate 1 compute node
 #SBATCH -n 1                      # total number of tasks
 #SBATCH --mem=1g                  # allocate 1 GB of memory
-#SBATCH -J "pytorch example"      # name of the job
-#SBATCH -o pytorch_example_%j.out # name of the output file
-#SBATCH -e pytorch_example_%j.err # name of the error file
+#SBATCH -J "MultiModalSentimentAnalysis"      # name of the job
+#SBATCH -o MultiModalSentimentAnalysis_%j.out # name of the output file
+#SBATCH -e MultiModalSentimentAnalysis_%j.err # name of the error file
 #SBATCH -p academic                  # partition to submit to
 #SBATCH -t 01:00:00               # time limit of 1 hour
 #SBATCH --gres=gpu:1              # request 1 GPU
 
-module load python/3.11.10               # These version were chosen for compatability with pytorch
-module load cuda/12.4.0/3mdaov5          # load CUDA (adjust if necessary)
+module load python/3.10.12
+module load miniconda3
+module load cuda
 
-python3 -m venv .venv         #
-source .venv/bin/activate     #
-pip install -r requirements.txt #
-python3 dataset_mvsa.py #
+# Create the conda environment if it doesn't exist
+if ! conda info --envs | grep -q mmsa_env; then
+    conda create -y -n mmsa_env python=3.11
+    source $(conda info --base)/etc/profile.d/conda.sh
+    conda activate mmsa_env
+    pip install -r requirements.txt
+else
+    source $(conda info --base)/etc/profile.d/conda.sh
+    conda activate mmsa_env
+fi
+
+python3 dataset_mvsa.py
