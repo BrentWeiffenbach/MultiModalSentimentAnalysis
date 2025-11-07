6463e6ee5468684d4b8d4c45d3b1382769c45a40
BrentWeiffenbach <rweiff2022@gmail.com>
2025-11-07 16:03:42 -0500

    renamed turing script

--- turing_test_script.sh
+++ turing_test_script.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1                      # allocate 1 compute node
 #SBATCH -n 1                      # total number of tasks
 #SBATCH --mem=8g                  # allocate 1 GB of memory
-#SBATCH -J "MultiModalSentimentAnalysis"      # name of the job
-#SBATCH -o MultiModalSentimentAnalysis_%j.out # name of the output file
-#SBATCH -e MultiModalSentimentAnalysis_%j.err # name of the error file
+#SBATCH -J "dataset_setup"      # name of the job
+#SBATCH -o dataset_setup%j.out # name of the output file
+#SBATCH -e dataset_setup%j.err # name of the error file
 #SBATCH -p academic                  # partition to submit to
 #SBATCH -t 01:00:00               # time limit of 1 hour
 #SBATCH --gres=gpu:1              # request 1 GPU
